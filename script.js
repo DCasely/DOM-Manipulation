59d23f2f8813b5c6e947158f196ad83b2a7909b6
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-06 17:38:18 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var tag = document.getElementById('highlight');
+
+var tags = document.getElementsByClassName('bolded');
+
+var tags = document.getElementsByTagName('li');
+var tags = document.getElementsByTagName('h1');
+var body = document.getElementsByTagName('body')[0];
+var tags = document.getElementsByTagName('head');
+
+var h1 = document.querySelector('h1');
+var li = document.querySelector('#highlight');
+var li = document.querySelector('.bolded');
+var li = document.querySelector('li a.special');
+
+var tags = document.querySelectorAll('h1');
+var lis = document.querySelectorAll('li');
+var bolded = document.querySelectorAll('.bolded');
+console.log(bolded);
+
+var li = document.querySelectorAll('#highlight');
+console.log(li);
