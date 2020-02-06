cbeee483f30f5616fbbf9e7b43333a42f75e2397
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-06 18:46:41 -0500

    Manipulating Style

---
+++ demo.js
@@ -0,0 +1,14 @@
+// var h1 = document.querySelector('h1');
+// h1.style.color = 'yellow';
+// h1.style.border = '5px solid pink';
+// console.log(h1.style);
+
+var p = document.querySelector('p');
+console.log(p);
+
+p.classList.add('big');
+p.classList.remove('big');
+p.classList.toggle('big');
+p.classList.toggle('big');
+
+console.log(p.classList);
