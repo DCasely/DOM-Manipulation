962e6e5f2a841f8b13796d7ae1379382100a350f
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-07 11:15:49 -0500

    Google website Manipulation

--- demo.js
+++ demo.js
@@ -11,3 +11,26 @@
 // p.classList.toggle('big');
 // p.classList.toggle('big');
 // console.log(p.classList);
+
+var img1 = document.getElementsByTagName('img')[0];
+console.log(img1);
+
+img1.getAttribute('src');
+
+img1.setAttribute(
+  'src',
+  'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+);
+
+var img2 = document.getElementsByTagName('img')[1];
+
+img2.setAttribute(
+  'src',
+  'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+);
+
+var a = document.querySelector('a');
+console.log(a);
+
+a.setAttribute('href', 'http://www.bing.com');
+a.textContent = 'LINK TO BING';
